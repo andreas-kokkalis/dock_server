8d86787c2d2084e52d4fcb3ac39a587dbb1914d2
Andreas Kokkalis <andreas.kokkalis@blocket.se>
2016-11-29 20:59:07 +0100

    Only return imaged of specific repo

--- dc/dc_list_images.go
+++ dc/dc_list_images.go
@@ -3,9 +3,12 @@ package dc
 import (
 	"context"
 	"fmt"
+	"strings"
 
 	"github.com/docker/docker/api/types"
 )
+
+const imageRepo = "sspreitzer/shellinabox"
 
 // ListImages retrieves the list of docker images from Docker Engine
 // via the Docker Remote API.
@@ -16,18 +19,33 @@ func ListImages() ([]Img, error) {
 	// Get list of images from Docker Engine
 	// types.ImageListOptions accepts filters.
 	// Since no filters are used, all images are returned.
-	// XXX: consider limiting this to only the original base image.
+	// XXX: consider limiting this to only the original base image. ==> NOT SUPPORTED
+
+	// the args will be {"image.name":{"ubuntu":true},"label":{"label1=1":true,"label2=2":true}}
+	//map[string]map[string]bool
+
+	// From the docker daemon source code:
+	/*var acceptedImageFilterTags = map[string]bool{
+		"dangling":  true,
+		"label":     true,
+		"before":    true,
+		"since":     true,
+		"reference": true,
+	}
+	*/
+
 	images, err := Cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		return imageList, err
 	}
 
-	// Extract imageID, RepoTags
-	imageList = make([]Img, len(images))
-	for i, image := range images {
-
-		fmt.Println(image)
-		imageList[i] = Img{ID: image.ID[7:19], RepoTags: image.RepoTags}
+	// Extract imageID, RepoTags for specific type of images
+	for _, image := range images {
+		fmt.Println(image.RepoTags[0])
+		s := image.RepoTags[0]
+		if s[0:strings.LastIndex(s, ":")] == imageRepo {
+			imageList = append(imageList, Img{ID: image.ID[7:19], RepoTags: image.RepoTags})
+		}
 	}
 	return imageList, nil
 }
