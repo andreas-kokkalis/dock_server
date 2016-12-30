734214fa19243b8c3c6725ede77ca08907c11204
Andreas Kokkalis <andreas.kokkalis@blocket.se>
2016-12-30 21:29:12 +0200

    missing file in 8e78ec8105ba117e76e8c2ae43e63b58a852f686

--- dc/dc_api_client.go
+++ dc/dc_api_client.go
@@ -3,27 +3,17 @@ package dc
 import (
 	"os"
 
-	"github.com/andreas-kokkalis/dock-server/conf"
 	"github.com/docker/docker/client"
 )
 
 // Cli the global docker api client
 var Cli *client.Client
 
-// ClientInit initializes a new client API variable that is globally
-// accessible when invoking this package.
+// APIClientInit initializes a new Docker API client.
 func APIClientInit(apiVersion string, dockerHost string) {
 
-	if apiVersion == "" {
-		os.Setenv("DOCKER_API_VERSION", conf.GetVal("dc.docker.api.host"))
-	} else {
-		os.Setenv("DOCKER_API_VERSION", apiVersion)
-	}
-	if dockerHost == "" {
-		os.Setenv("DOCKER_HOST", conf.GetVal("dc.docker.api.version"))
-	} else {
-		os.Setenv("DOCKER_HOST", dockerHost)
-	}
+	os.Setenv("DOCKER_API_VERSION", apiVersion)
+	os.Setenv("DOCKER_HOST", dockerHost)
 
 	cli, err := client.NewEnvClient()
 	if err != nil {
