9487d25974d356cba43ea423c7a466464a7b326c
Andreas Kokkalis <andreas.kokkalis@schibsted.com>
2017-12-18 13:43:09 -0500

    encrypt travis env vars for docker hub

--- scripts/travis/integration-setup.sh
+++ scripts/travis/integration-setup.sh
@@ -2,5 +2,8 @@
 
 set -ex
 
-docker login -u $DOCKER_HUB_USER -p $DOCKER_HUB_PWD
+: ${DOCKER_USER?"Set the environment variable DOCKER_USER"}
+: ${DOCKER_PWD?"Set the environment variable DOCKER_PWD"}
+
+docker login -u ${DOCKER_USER} -p ${DOCKER_PWD}
 docker pull andreaskokkalis/dc --all-tags
