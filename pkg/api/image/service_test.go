e2101a244867a1651655c4aa1a71b77462bef6d9
Andreas Kokkalis <andrewkokkalis@gmail.com>
2017-12-27 16:03:40 -0500

    add unit tests for Image service routes (#18)

--- pkg/api/image/service_test.go
+++ pkg/api/image/service_test.go
@@ -1,40 +1,197 @@
 package image
 
-// var configDir = "../../../conf/"
+import (
+	"encoding/json"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 
-/*
+	"github.com/andreas-kokkalis/dock_server/pkg/api"
+	"github.com/andreas-kokkalis/dock_server/pkg/api/repositories/repomocks"
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
 func TestListImages(t *testing.T) {
-	assert := assert.New(t)
-	// Initialize the configuration manager
-	var c config.Config
-	c, err = config.NewConfig(configDir, vars.Mode)
-	if err != nil {
-		log.Fatal(err)
+	imgList := []api.Img{
+		api.Img{
+			ID:       "12345",
+			RepoTags: []string{"ref"},
+		},
+	}
+	tests := []struct {
+		service        Service
+		expectCode     int
+		expectResponse interface{}
+		name           string
+	}{
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithImageList(imgList, nil)),
+			expectCode:     http.StatusOK,
+			expectResponse: imgList,
+			name:           "list images",
+		},
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithImageList([]api.Img{}, errors.New("error"))),
+			expectCode:     http.StatusInternalServerError,
+			expectResponse: nil,
+			name:           "list images error",
+		},
 	}
 
-	router := httprouter.New()
-	router.GET("/v0/admin/images", ListImages)
-	r, err := http.NewRequest("GET", "/v0/admin/images", nil)
-	if err != nil {
-		t.Fatal(err)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", nil)
+			tt.service.ListImages(w, r, nil)
+			assert.Equal(t, tt.expectCode, w.Code)
+
+			var actualResponse api.Response
+			assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &actualResponse))
+			if tt.expectCode == http.StatusOK {
+				byteData, _ := json.Marshal(actualResponse.Data)
+				var actualData []api.Img
+				assert.NoError(t, json.Unmarshal(byteData, &actualData))
+				assert.Equal(t, tt.expectResponse, actualData)
+			} else {
+				assert.NotEmpty(t, actualResponse.Errors)
+			}
+		})
 	}
-	w := httptest.NewRecorder()
-	router.ServeHTTP(w, r)
-
-	assert.Equal(w.Code, http.StatusOK, "It should be equal")
-
-	decoder := json.NewDecoder(w.Body)
-	var data Response
-	err = decoder.Decode(&data)
-	assert.Equal(nil, err, "It should not return an error")
-
-	resp := w.Result()
-	defer resp.Body.Close()
-	bodyBytes, _ := ioutil.ReadAll(resp.Body)
-	bodyString := string(bodyBytes)
-	t.Logf("%s", bodyString)
-
-	fmt.Printf("LIST_IMAGES:\t %d - %s", w.Code, w.Body.String())
-
 }
-*/
+
+func TestGetImageHistory(t *testing.T) {
+	imgHistory := []api.ImgHistory{
+		api.ImgHistory{
+
+			Comment:   "foo",
+			CreatedBy: "bar",
+			ID:        "83364c85cafc",
+			RepoTags:  []string{"ref"},
+			Size:      1,
+		},
+	}
+	tests := []struct {
+		service        Service
+		expectCode     int
+		params         httprouter.Params
+		expectResponse interface{}
+		name           string
+	}{
+		{
+			service:        NewService(nil, repomocks.NewDockerRepositoryMock()),
+			expectCode:     http.StatusBadRequest,
+			params:         nil,
+			expectResponse: imgHistory,
+			name:           "image history wrong params",
+		},
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithImageHistory([]api.ImgHistory{}, errors.New("error"))),
+			expectCode:     http.StatusInternalServerError,
+			params:         []httprouter.Param{{Key: "id", Value: "83364c85cafc"}},
+			expectResponse: nil,
+			name:           "image history docker error",
+		},
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithImageHistory(imgHistory, nil)),
+			expectCode:     http.StatusOK,
+			params:         []httprouter.Param{{Key: "id", Value: "83364c85cafc"}},
+			expectResponse: imgHistory,
+			name:           "image history good",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", nil)
+			tt.service.GetImageHistory(w, r, tt.params)
+			assert.Equal(t, tt.expectCode, w.Code)
+
+			var actualResponse api.Response
+			assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &actualResponse))
+			if tt.expectCode == http.StatusOK {
+				byteData, _ := json.Marshal(actualResponse.Data)
+				var actualData []api.ImgHistory
+				assert.NoError(t, json.Unmarshal(byteData, &actualData))
+				assert.Equal(t, tt.expectResponse, actualData)
+			} else {
+				assert.NotEmpty(t, actualResponse.Errors)
+			}
+		})
+	}
+}
+
+func TestRemoveImage(t *testing.T) {
+	tests := []struct {
+		service        Service
+		expectCode     int
+		params         httprouter.Params
+		expectResponse interface{}
+		name           string
+	}{
+		{
+			service:        NewService(nil, repomocks.NewDockerRepositoryMock()),
+			expectCode:     http.StatusBadRequest,
+			params:         nil,
+			expectResponse: nil,
+			name:           "remove image wrong image id",
+		},
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithGetRunningContainersByImageID([]api.Ctn{api.Ctn{}}, nil)),
+			expectCode:     http.StatusBadRequest,
+			params:         []httprouter.Param{{Key: "id", Value: "83364c85cafc"}},
+			expectResponse: nil,
+			name:           "image has running containers cannot delete",
+		},
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithGetRunningContainersByImageID([]api.Ctn{}, nil).
+					WithImageRemove(errors.New("error"))),
+			expectCode:     http.StatusInternalServerError,
+			params:         []httprouter.Param{{Key: "id", Value: "83364c85cafc"}},
+			expectResponse: nil,
+			name:           "image remove docker error",
+		},
+		{
+			service: NewService(nil,
+				repomocks.NewDockerRepositoryMock().
+					WithGetRunningContainersByImageID([]api.Ctn{}, nil).
+					WithImageRemove(nil)),
+			expectCode:     http.StatusOK,
+			params:         []httprouter.Param{{Key: "id", Value: "83364c85cafc"}},
+			expectResponse: "Image was removed successfully",
+			name:           "image remove ok",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			w := httptest.NewRecorder()
+			r := httptest.NewRequest(http.MethodGet, "/", nil)
+			tt.service.RemoveImage(w, r, tt.params)
+			assert.Equal(t, tt.expectCode, w.Code)
+
+			var actualResponse api.Response
+			assert.NoError(t, json.Unmarshal(w.Body.Bytes(), &actualResponse))
+			if tt.expectCode == http.StatusOK {
+				byteData, _ := json.Marshal(actualResponse.Data)
+				var actualData string
+				assert.NoError(t, json.Unmarshal(byteData, &actualData))
+				assert.Equal(t, tt.expectResponse, actualData)
+			} else {
+				assert.NotEmpty(t, actualResponse.Errors)
+			}
+		})
+	}
+}
